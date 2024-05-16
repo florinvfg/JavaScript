905fd7232d5e1e5445913a3a054893fa5e104c6b
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-05-16 10:14:34 +0200

    Add files via upload

--- javaScript/src/main/webapp/horarios.jsp
+++ javaScript/src/main/webapp/horarios.jsp
@@ -14,12 +14,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="script/horarios.js"></script>
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/style.css">
+    <script src="script/horarios.js"></script>
 </head>
 <body>
 <div class="contenedor1 col-6">
+    <div class="cajaFlex1 col-12">
     <button class="boton" value="l">L</button>
     <button class="boton" value="m">M</button>
     <button class="boton" value="x">X</button>
@@ -27,13 +28,14 @@
     <button class="boton" value="v">V</button>
     <button class="boton" value="s">S</button>
     <button class="boton" value="d">D</button>
+    </div>
 
+<div class="cajaFlex col-12">
+    <p class="horario centrado col-12"></p>
 
-    <p class="horario centrado"></p>
+        </div>
+    </div>
 
-
-
-</div>
 
 </body>
 </html>
--- javaScript/src/main/webapp/script/horarios.js
+++ javaScript/src/main/webapp/script/horarios.js
@@ -1,5 +1,46 @@
 window.onload=function (){
+let horario="";
+let botones=document.querySelectorAll(".boton");//toma todos los elementos que tiene la clase boton y crea un array
 
+    //array.forEach(arrayIntem=>{......})
+    botones.forEach (pinchiado=>{
+        console.log(pinchiado.value)
+        pinchiado.addEventListener("click",function(){
+            switch (pinchiado.value){
+                case "l":{
+                    horario="Lunes: De 9:00 a 14 | 16:00 a 19:00";
+                    break;
+                }
+                case "m":{
+                    horario="Martes: De 9:00 a 14 | 16:00 a 19:00";
+                    break;
+                }
+                case "x":{
+                    horario="Miercoles: De 9:00 a 14 | 16:00 a 19:00";
+                    break;
+                }
+                case "j":{
+                    horario="Jueves: De 9:00 a 14 | 16:00 a 19:00";
+                    break;
+                }
+                case "v":{
+                    horario="Viernes: De 9:00 a 14 | 16:00 a 19:00";
+                    break;
+                }
+                case "s":{
+                    horario="sabado: De 9:00 a 14 ";
+                    break;
+                }
+                case "d":{
+                    horario="Domingo: Cerrado";
+                    break;
+                }
+            }
+            document.querySelector(".horario").innerHTML="<h2>Horario</h2>"+horario;
+
+
+        })
+    })
 
 
 }
